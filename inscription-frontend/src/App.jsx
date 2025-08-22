9b708795fe35045b10fda137f03a3e42b36dcd93
Michael <ngaleumike1234@outlook.com>
2025-08-22 14:09:29 +0100

    Fix the student dashboard layout

--- inscription-frontend/src/App.jsx
+++ inscription-frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Outlet, useNavigate } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import AppHeader from './components/layout/AppHeader';
 import AppFooter from './components/layout/AppFooter';
 import HomePage from './pages/HomePage';
@@ -8,7 +8,6 @@ import ForgotPasswordPage from './pages/ForgotPasswordPage';
 import VerifyEmailPage from './pages/VerifyEmailPage';
 import ProtectedRoute from './components/ProtectedRoute';
 import CourseDetail from './pages/CourseDetail';
-
 
 // Admin Dashboard Imports
 import AdminDashboardLayout from './components/adminDashboard/AdminDashboardLayout';
@@ -22,13 +21,11 @@ import UserForm from './components/adminDashboard/UserForm';
 import AdminFAQ from './components/adminDashboard/AdminFAQ';
 import AdminHelp from './components/adminDashboard/AdminHelp';
 
-
 // Student Dashboard Imports
 import StudentDashboardLayout from './components/studentDashboard/StudentDashboardLayout';
 import StudentDashboardContent from './components/studentDashboard/StudentDashboardContent';
 import StudentFAQ from './components/studentDashboard/StudentFAQ';
 import StudentHelp from './components/studentDashboard/StudentHelp';
-
 
 const AppLayoutWithLargeFooter = () => (
     <>
@@ -47,16 +44,6 @@ const AppLayoutWithDefaultFooter = () => (
             <Outlet />
         </main>
         <AppFooter variant="default" />
-    </>
-);
-
-// New dashboard layout component
-const DashboardLayout = () => (
-    <>
-        <AppHeader />
-        <div className="flex flex-1">
-            <Outlet />
-        </div>
     </>
 );
 
@@ -85,28 +72,26 @@ function App() {
                     <Route path="/verify-email" element={<VerifyEmailPage />} />
                     <Route path="/courses/:courseName" element={<CourseDetail />} />
                 </Route>
-                
-                {/* Dashboard routes nested within the new DashboardLayout component */}
-                <Route element={<DashboardLayout />}>
-                    <Route path="/dashboard" element={<ProtectedRoute allowedRoles={['student']}><StudentDashboardLayout /></ProtectedRoute>}>
-                        <Route index element={<StudentDashboardContent />} />
-                        <Route path="faq" element={<StudentFAQ />} />
-                        <Route path="help" element={<StudentHelp />} />
-                    </Route>
-                    
-                    <Route path="/admin-dashboard" element={<ProtectedRoute allowedRoles={['admin']}><AdminDashboardLayout /></ProtectedRoute>}>
-                        <Route index element={<AdminDashboardContent />} />
-                        <Route path="enrollment-management" element={<StudentEnrollmentManagement onViewDetails={handleViewDetails} />} />
-                        <Route path="enrollment-management/:enrollmentId" element={<StudentEnrollmentDetails onBack={handleBack} />} />
-                        <Route path="program-management" element={<ProgramManagement />} />
-                        <Route path="program-management/add" element={<ProgramForm />} />
-                        <Route path="program-management/edit/:id" element={<ProgramForm />} />
-                        <Route path="user-management" element={<UserManagement />} />
-                        <Route path="user-management/add" element={<UserForm />} />
-                        <Route path="user-management/edit/:id" element={<UserForm />} />
-                        <Route path="faq" element={<AdminFAQ />} />
-                        <Route path="help" element={<AdminHelp />} />
-                    </Route>
+
+                {/* Dashboard routes nested directly within ProtectedRoute */}
+                <Route path="/dashboard" element={<ProtectedRoute allowedRoles={['student']}><StudentDashboardLayout /></ProtectedRoute>}>
+                    <Route index element={<StudentDashboardContent />} />
+                    <Route path="faq" element={<StudentFAQ />} />
+                    <Route path="help" element={<StudentHelp />} />
+                </Route>
+
+                <Route path="/admin-dashboard" element={<ProtectedRoute allowedRoles={['admin']}><AdminDashboardLayout /></ProtectedRoute>}>
+                    <Route index element={<AdminDashboardContent />} />
+                    <Route path="enrollment-management" element={<StudentEnrollmentManagement onViewDetails={handleViewDetails} />} />
+                    <Route path="enrollment-management/:enrollmentId" element={<StudentEnrollmentDetails onBack={handleBack} />} />
+                    <Route path="program-management" element={<ProgramManagement />} />
+                    <Route path="program-management/add" element={<ProgramForm />} />
+                    <Route path="program-management/edit/:id" element={<ProgramForm />} />
+                    <Route path="user-management" element={<UserManagement />} />
+                    <Route path="user-management/add" element={<UserForm />} />
+                    <Route path="user-management/edit/:id" element={<UserForm />} />
+                    <Route path="faq" element={<AdminFAQ />} />
+                    <Route path="help" element={<AdminHelp />} />
                 </Route>
             </Routes>
         </div>
